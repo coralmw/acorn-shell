d1e77c0e2bb20c1b8f9c6ba548b1b7f0a2bf472a
Thomas Parks <thomasparks@outlook.com>
2020-07-28 22:42:51 +0100

    fix liblitepcie wrapper import.

--- tools/liblitepcie/liblitepcie.i
+++ tools/liblitepcie/liblitepcie.i
@@ -4,4 +4,6 @@
 #include "../../build/acorn_cle_215/driver/user/liblitepcie.h"
 %}
 
-%include "../../build/acorn_cle_215/driver/user/liblitepcie.h"
+extern uint32_t litepcie_readl(int fd, uint32_t addr);
+extern void litepcie_writel(int fd, uint32_t addr, uint32_t val);
+extern void litepcie_reload(int fd);
--- tools/liblitepcie/setup.py
+++ tools/liblitepcie/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup, Extension
 
-extension_mod = Extension("_liblitepcie", ["liblitepcie.cc", "../../build/acorn_cle_215/driver/user/liblitepcie.c"],
-                          include_dirs=["../../build/acorn_cle_215/driver/user/", "../../build/acorn_cle_215/driver/kernel/"])
+extension_mod = Extension("_liblitepcie", ["liblitepcie.c", "../../build/acorn_cle_215/driver/user/liblitepcie.c"],
+                          include_dirs=["../../build/acorn_cle_215/driver/user/", "../../build/acorn_cle_215/driver/kernel/"],
+                          language="c")
 
 setup(name = "liblitepcie", ext_modules=[extension_mod])
